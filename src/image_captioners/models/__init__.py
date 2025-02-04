eb0dcc8bb870483505e3390d29186e0c16587760
CodeKnight314 <tang.ri@northeastern.edu>
2025-02-04 17:37:32 +0000

    Add ViTGPT2Model and VITGPT2Processor to image captioners models module

--- src/image_captioners/models/__init__.py
+++ src/image_captioners/models/__init__.py
@@ -1,11 +1,12 @@
 from base import BaseModel, BaseProcessor
 from llava import LlavaModel, LlavaProcessor
+from vit_gpt2 import ViTGPT2Model, VITGPT2Processor
 
 __all__ = [
     "BaseModel",
     "BaseProcessor",
     "LlavaModel",
-    "LlavaProcessor"
+    "LlavaProcessor", 
+    "ViTGPT2Model", 
+    "VITGPT2Processor"
 ]
-
-__version__ = "0.1.0"
