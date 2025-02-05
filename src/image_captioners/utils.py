5ddd1827dbeffaf218c65b6d23eabce85579a5a1
CodeKnight314 <tang.ri@northeastern.edu>
2025-02-05 14:16:04 +0000

    Refactor save_images function to handle captions and save to structured directories; add save_captions function for CSV output; improve error handling in load_config

--- src/image_captioners/utils.py
+++ src/image_captioners/utils.py
@@ -1,23 +1,40 @@
 import os
-import importlib
 from tqdm import tqdm
 import yaml
+from typing import Dict, List
             
-def save_images(images: list, save_pth: str, prompts: list = None):
+def save_images(captions: List[Dict[str, str]], output_path: str = "output/samples/"):
     """
     Save a list of images to a directory.
     
     Args:
-        images (list): List of PIL images to save.
-        save_pth (str): Path to save the images.
+        captions (List[Dict[str, str]]): List of images to save.
+        output_path (str): Path to save the images.
     """
-    os.makedirs(save_pth, exist_ok=True)
-    for i, img in enumerate(tqdm(images, total=len(images), desc=f"Saving images to {save_pth}")):
-        if prompts is not None: 
-            with open(os.path.join(save_pth, f"result_{i}.txt"), 'w') as f:
-                f.write(prompts[i])
-        img.save(os.path.join(save_pth, f"result_{i}.png"))        
-    print("Finished saving images")
+    os.makedirs(output_path, exist_ok=True)
+    for i, img in enumerate(tqdm(captions, total=len(captions), desc=f"Saving images to {output_path}")):
+        with open(os.path.join(output_path, "captions", f"result_{i}.txt"), 'w') as f:
+            f.write(img["caption"])
+        img.save(os.path.join(output_path, "images", f"result_{i}.png"))
+    print("[INFO] Finished saving images")
+    
+def save_captions(captions: List[Dict[str, str]], output_path: str = "output"):
+    """
+    Save a list of dictionaries to a csv file.
+    
+    Args:
+        captions (List[Dict[str, str]]): List of dictionaries to save.
+        output_path (str): Path to save the csv file.
+    """
+    os.makedirs(output_path, exist_ok=True)
+    file_name = os.path.join(output_path, "captions.csv")
+    
+    with open(file_name, 'w') as f: 
+        f.write(','.join(captions[0].keys()) + '\n')
+        f.write('\n')
+        for row in captions: 
+            f.write(','.join(str(x) for x in row.values()) + '\n')
+    print(f"Captions saved to {file_name}")
 
 def load_config(config_path: str):
     """
@@ -26,5 +43,10 @@ def load_config(config_path: str):
     Args:
         config_path (str): Path to the configuration file.
     """
-    with open(config_path, "r") as f:
-        return yaml.safe_load(f)
+    if(not os.path.exists(config_path)):
+        raise ValueError(f"[ERROR] Config file {config_path} not found.")
+    try:
+        with open(config_path, "r") as f:
+            return yaml.safe_load(f)
+    except Exception as e:
+        raise Exception(f"[ERROR] Error loading config file {config_path}: {e}")
